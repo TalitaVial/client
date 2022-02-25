3d5d310c7ac550b930ebb39489ce1d8674885540
Talita Vial <talitavialsilva1@gmail.com>
2022-02-24 22:24:00 -0400

    feat controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,6 +1,15 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document)
+    this.InputData = $('#data')
+    this.InputQuantidade = $('#quantidade')
+    this.InputValor = $('#valor')
+  }
   adiciona(event) {
     event.preventDefault()
-    alert('Foi chamado')
+
+    console.log(this.InputData.value)
+    console.log(this.InputQuantidade.value)
+    console.log(this.InputValor.value)
   }
 }
